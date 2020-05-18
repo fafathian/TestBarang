a21b26eeb1b85d8ff88099395a8f258e7b781ef1
fafathian <zulfa.fathian.ft18@mail.umy.ac.id>
2020-05-18 16:14:27 +0700

    Done

---
+++ app/src/main/java/com/example/testbarang/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.testbarang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.security.PublicKey;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bTambah;
+    private Button bLihat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bLihat = (Button) findViewById(R.id.btnLihat);
+        bTambah = (Button) findViewById(R.id.btnTambah);
+
+        bTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(TambahData.getActIntent(MainActivity.this));
+            }
+        });
+        bLihat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
